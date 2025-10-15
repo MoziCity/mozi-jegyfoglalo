a73d092267c5c1d7335c2ab6f6d3ac52f0fc2504
viktor <viktor@viktor.com>
2025-10-15 10:29:12 +0200

    Initial commit

---
+++ src/test/java/com/MoziCity/mozi_jegyfoglalo/MoziJegyfoglaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoziCity.mozi_jegyfoglalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoziJegyfoglaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
